0a4390a9adc01de64b1526e1434d51a96fb724a2
Arne Wittmaack <arne.wittmaack@web.de>
2024-05-28 10:51:04 +0200

    Add health check

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, UploadFile, File, Depends
+from fastapi import APIRouter, UploadFile, File, Depends, status
+from pydantic import BaseModel
 from app.models import WhisperTranscriber
 from app.utils import handle_transcription
 
@@ -17,3 +18,27 @@ async def transcribe_audio(file: UploadFile = File(...), transcriber: WhisperTra
     - JSONResponse: A JSON response containing the transcript text.
     """
     return await handle_transcription(file, transcriber)
+
+class HealthCheck(BaseModel):
+    """Response model to validate and return when performing a health check."""
+    status: str = "OK"
+
+@router.get(
+    "/health",
+    tags=["healthcheck"],
+    summary="Perform a Health Check",
+    response_description="Return HTTP Status Code 200 (OK)",
+    status_code=status.HTTP_200_OK,
+    response_model=HealthCheck,
+)
+def get_health() -> HealthCheck:
+    """
+    ## Perform a Health Check
+    Endpoint to perform a healthcheck on. This endpoint can primarily be used Docker
+    to ensure a robust container orchestration and management is in place. Other
+    services which rely on proper functioning of the API service will not deploy if this
+    endpoint returns any other HTTP status code except 200 (OK).
+    Returns:
+        HealthCheck: Returns a JSON response with the health status
+    """
+    return HealthCheck(status="OK")
