682dcf1b523dc68b45c21ad44920615f77d4d993
Arne Wittmaack <arne.wittmaack@web.de>
2024-05-28 10:49:55 +0200

    Add config file

--- app/main.py
+++ app/main.py
@@ -1,19 +1,24 @@
+from configparser import ConfigParser
 from fastapi import FastAPI
-from app.routes import router
 from app.models import WhisperTranscriber
+from app.routes import router
 
-def create_app(model_size: str = "large") -> FastAPI:
+def create_app(config: ConfigParser) -> FastAPI:
     """
     Create and configure an instance of the FastAPI application.
 
     Parameters:
-    - model_size (str): The size of the Whisper model to load (e.g., 'tiny', 'base', 'small', 'medium', 'large').
+    - config (ConfigParser): ConfigParser instance containing the configuration for the service.
 
     Returns:
     - FastAPI: The configured FastAPI application instance.
     """
     app = FastAPI()
-    app.dependency_overrides[WhisperTranscriber] = lambda: WhisperTranscriber(model_size)
+    # Include transcription service
+    whisper_size = config['WhisperSize']
+    app.dependency_overrides[WhisperTranscriber] = lambda: WhisperTranscriber(whisper_size)
+
+    # Add routes to service
     app.include_router(router)
     return app
 
@@ -24,14 +29,17 @@ def main():
     import argparse
     import uvicorn
 
-    parser = argparse.ArgumentParser(description="Run a transcription server using Whisper model.")
-    parser.add_argument('--model-size', type=str, default="large", help="Size of the Whisper model to load (e.g., tiny, base, small, medium, large).")
+    config = ConfigParser()
+    config.read('config.ini')
+
+    parser = argparse.ArgumentParser(description="Run your own AI services.")
     parser.add_argument('--host', type=str, default="0.0.0.0", help="Host to run the server on.")
     parser.add_argument('--port', type=int, default=8000, help="Port to run the server on.")
+    parser.add_argument('--config', type=str, default="default", help="Name of configuration file during use.")
     
     args = parser.parse_args()
     
-    app = create_app(model_size=args.model_size)
+    app = create_app(config=config[args.config])
     uvicorn.run(app, host=args.host, port=args.port)
 
 if __name__ == "__main__":
